dd9f1f72ef5e1aa69a1826040ce4a5ffbb5fbf61
Lemper29 <buumhalal@gmail.com>
2026-03-01 21:07:45 +0300

    feat: add handeshake

--- p2p/tcp_transport.go
+++ p2p/tcp_transport.go
@@ -25,6 +25,8 @@ func NewTCPPeer(conn net.Conn, outbound bool) *TCPPeer {
 type TCPTransport struct {
 	listenAddress string
 	listener      net.Listener
+	shakeHands    HandshakeFunc
+	decoder       Decoder
 
 	mu    sync.RWMutex
 	peers map[net.Addr]Peer
@@ -32,6 +34,7 @@ type TCPTransport struct {
 
 func NewTCPTransport(listenAddr string) *TCPTransport {
 	return &TCPTransport{
+		shakeHands:    NOPHandshakeFunc,
 		listenAddress: listenAddr,
 	}
 }
@@ -55,11 +58,27 @@ func (t *TCPTransport) startAcceptLoop() {
 		if err != nil {
 			fmt.Printf("TCP accept error: %s\n", err)
 		}
+
+		fmt.Printf("new incoming connection: %+v\n", conn)
+
 		go t.handleConn(conn)
 	}
 }
 
+type Temp struct{}
+
 func (t *TCPTransport) handleConn(conn net.Conn) {
 	peer := NewTCPPeer(conn, true)
-	fmt.Printf("new incoming connection: %+v\n", peer)
+
+	if err := t.shakeHands(peer); err != nil {
+	}
+
+	msg := &Temp{}
+
+	for {
+		if err := t.decoder.Decoder(conn, msg); err != nil {
+			fmt.Printf("TCP error: %s\n", err)
+			continue
+		}
+	}
 }
